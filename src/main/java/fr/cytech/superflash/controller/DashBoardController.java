65fde3610b7d6b5f28828ed341f5fff31be0f05c
simoninformatico <loossimon@cy-tech.fr>
2023-10-30 13:40:53 +0200

    controller dachboard deck

--- src/main/java/fr/cytech/superflash/controller/DashBoardController.java
+++ src/main/java/fr/cytech/superflash/controller/DashBoardController.java
@@ -9,27 +9,33 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.cytech.superflash.repository.MatiereRepository;
 import fr.cytech.superflash.entity.Matiere;
 
+import fr.cytech.superflash.entity.Deck;
+import fr.cytech.superflash.repository.DeckRepository;
+
 @Controller
 public class DashBoardController {
 
-
-
-
     @Autowired
     private MatiereRepository matiereRepository;
+    private DeckRepository deckRepository;
 
     @GetMapping("/main")
     public String dash(Model model) {
 
         List<Matiere> matieres = matiereRepository.findAll();
         model.addAttribute("matieres", matieres);
+        List<Deck> decks = deckRepository.findAll();
+        model.addAttribute("decks",decks);
         return "dashboard";
     }
 
-    
+  
+
 }
--- src/main/java/fr/cytech/superflash/entity/Score.java
+++ src/main/java/fr/cytech/superflash/entity/Score.java
@@ -1,5 +1,20 @@
 package fr.cytech.superflash.entity;
 
+import java.util.Date;
+import jakarta.persistence.*;
+
+@Entity
 public class Score {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @ManyToOne
+    private Deck deck;
+
+    private int score;
+
 }
